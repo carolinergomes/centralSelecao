54cc98e825ffe86cc333af38caa48fe24848e7f6
Carol Gomes <caroline.gomes@estudante.ifms.edu.br>
2024-11-08 21:07:56 -0300

    configuracao do inertia

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+      <h1>Welcome to Laravel 11 with Inertia.js and Vue.js</h1>
+    </div>
+  </template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/oi', function () {
+    return Inertia::render('Welcome');
 });
